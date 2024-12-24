bca7b8e8402d0342c01360fed206a69d7313fd16
Devin Alexander <devinmalexander@gmail.com>
2024-12-24 16:42:06 -0600

    Get fly working by pressing keyboard

---
+++ scenes/plane/plane.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+
+const GRAVITY: float = 600.0
+const POWER: float = -200.0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	
+	# makes the plane fall
+	velocity.y += GRAVITY * delta
+	
+	fly()
+	
+	move_and_slide()
+	
+func fly() -> void:
+	if Input.is_action_just_pressed("fly") == true:
+		velocity.y = POWER
+		
+		
+		
+		
+		
