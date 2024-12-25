9edbeb872443dd0c211a2f6aa48d582ff651fee7
Devin Alexander <devinmalexander@gmail.com>
2024-12-25 16:32:46 -0600

    Autoload (Globals / pretend singletons)

--- scenes/pipes/pipes.gd
+++ scenes/pipes/pipes.gd
@@ -1,14 +1,17 @@
 extends Node2D
 class_name Pipes
 
+const OFF_SCREEN: float = -500.0
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	# move the pipe
-	position.x -= delta * 120.0
+	position.x -= delta * GameManager.SCROLL_SPEED
+	check_off_screen()
 
 # will handle if notifier doesn't fire off
 func check_off_screen() -> void:
-	if position.x < -500:
+	if position.x < OFF_SCREEN:
 		queue_free()
 
 
