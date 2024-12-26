8fb6b95c791b6186c628fe8c54362fa2960c61b1
Devin Alexander <devinmalexander@gmail.com>
2024-12-26 06:46:34 -0600

    Saving scores

--- singletons/score_manager.gd
+++ singletons/score_manager.gd
@@ -1,12 +1,14 @@
 extends Node
 
+const SCORES_PATH = "user://tappy.dat"
+
 
 var _score: int = 0
 var _high_score: int = 0
 
 
 func _ready() -> void:
-	pass 
+	load_high_score() 
 
 
 func get_score() -> int:
@@ -21,8 +23,28 @@ func set_score(v: int) -> void:
 	_score = v
 	if _score > _high_score:
 		_high_score = _score
+		save_high_score_to_file()
 	SignalManager.on_score_updated.emit(_score)
 
 
 func increment_score() -> void:
 	set_score(_score + 1)
+
+
+func load_high_score() -> void:
+	var file: FileAccess = FileAccess.open(SCORES_PATH, FileAccess.READ)
+	if file:
+		if file.get_length() > 0:
+			_high_score = file.get_as_text().to_int()
+			print("Loaded HS")
+		else:
+			print("Nothing in file")
+		file.close()
+	else:
+		print("FAILED to load file")
+	
+func save_high_score_to_file() -> void:
+	var file: FileAccess = FileAccess.open(SCORES_PATH, FileAccess.WRITE)
+	if file:
+		file.store_string(str(_high_score))
+		file.close()
